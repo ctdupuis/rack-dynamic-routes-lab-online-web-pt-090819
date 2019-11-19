72bd8213ad97594045fc4eb1fa7f3b9852a95ebc
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-19 03:55:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
-    if req.path.match(/items/)
+    # if req.path.match(/items/)
       
-    else
+    # else
       
-    end
+    # end
   end
 end
