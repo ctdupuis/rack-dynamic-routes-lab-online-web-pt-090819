f76a8a0a97f3075baa1a9714144c91e755bc5a51
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-19 04:01:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item=
+      if item = @@items.find{|i| i
     else
       resp.write "Route not found"
       resp.status = 404
